c64c9289f3e47477c282dc6ac8ec48fac312758a
guz2001 <guz20dgc@gmail.com>
2025-11-05 16:55:57 -0500

    creacion de borrar tareas

--- scripts.js
+++ scripts.js
@@ -4,16 +4,44 @@ const todoListUL=document.getElementById('todo-list');
  let allTodos=[];
 todoform.addEventListener('submit',function(e){
     e.preventDefault();
-    alert('test');
+    addTodo();
 
 })
 function addTodo(){
     const todoText=todoinput.value;
-    alert(textText);
 
-}
-function addEventListener(){
 
+    //funcion para evitar tareas vacias
+    if(todoText.trim()==="") return;
+
+
+    // creacion de lista 
+    const newLi=document.createElement('li');
+    
+    
+    newLi.innerText =todoText;
+    
+    // agregamos a la lista
+
+    newLi.innerText= todoText;
+    
+    //limpiar input "entrada"
+    todoListUL.appendChild(newli);
+    todoinput.value= " ";
 }
+// variable para crear el boton de borrar elemento
+const borrarBoton=document.createElement('button');
+
+borrarBoton.innerText = 'X';//boton visual para borrar
+newLi.appendChild(borrarBoton);
+
+// funcionalidad para cuando se haga click se tache la tarea
+
+newLi.addEventListener('click',function(){
+
+    newLi.classList.toggle('completed')
+});
+
+
 
 
