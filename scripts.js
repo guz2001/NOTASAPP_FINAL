62a70467f84e678839d89165dbb074bd15d74cfa
guz2001 <guz20dgc@gmail.com>
2025-11-05 17:10:41 -0500

    funcionalidad agregar listas, borrar o tachar v2

--- scripts.js
+++ scripts.js
@@ -14,7 +14,20 @@ function addTodo(){
     //funcion para evitar tareas vacias
     if(todoText.trim()==="") return;
 
+    //Crear objeto  para guardar tareas
+    const todoObjt={
+        text: todoText,
+        completed:false
 
+    };
+
+    allTodos.push(todoObjt);// se guardan todas la tareas en el lista
+
+    saveToLocalStorage();//se guarda la lista completa en storage
+
+
+    renderTodos();//se dibuja toda la lista creadas
+    
     // creacion de lista 
     const newLi=document.createElement('li');
     
@@ -26,21 +39,22 @@ function addTodo(){
     newLi.innerText= todoText;
     
     //limpiar input "entrada"
-    todoListUL.appendChild(newli);
+    todoListUL.appendChild(newLi);
     todoinput.value= " ";
-}
-// variable para crear el boton de borrar elemento
-const borrarBoton=document.createElement('button');
+    // variable para crear el boton de borrar elemento
+    const borrarBoton=document.createElement('button');
 
-borrarBoton.innerText = 'X';//boton visual para borrar
-newLi.appendChild(borrarBoton);
+    borrarBoton.innerText = 'X';//boton visual para borrar
+    newLi.appendChild(borrarBoton);
 
-// funcionalidad para cuando se haga click se tache la tarea
+    // funcionalidad para cuando se haga click se tache la tarea
 
-newLi.addEventListener('click',function(){
+    newLi.addEventListener('click',function(){
 
     newLi.classList.toggle('completed')
 });
+}
+
 
 
 
